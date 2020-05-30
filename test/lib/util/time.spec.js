78d60afa45ad7a0a04e17e13e9211c9c4477b4de
Ryan Roemer <ryan.roemer@formidable.com>
2020-05-30 14:44:20 -0700

    WIP: Start time util tests

--- test/lib/util/time.spec.js
+++ test/lib/util/time.spec.js
@@ -1,7 +1,30 @@
 "use strict";
 
+/* eslint-disable no-magic-numbers*/
+
+const { padNum, toDateNum, toDatePath } = require("../../../lib/util/time");
+
 describe("lib/util/time", () => {
-  describe("getDateNum", () => {
+  describe("padNum", () => {
+    it("handles base cases", () => {
+      expect(padNum(0, 1)).to.eql("1");
+      expect(padNum(1, 1)).to.eql("1");
+      expect(padNum(1, 123)).to.eql("123");
+    });
+
+    it("handles padded cases", () => {
+      expect(padNum(2, 1)).to.eql("01");
+      expect(padNum(4, 1)).to.eql("0001");
+      expect(padNum(10, 123)).to.eql("0000000123");
+    });
+  });
+
+  describe("toDateNum", () => {
+    it("TODO: base cases");
+    it("TODO: general cases");
+  });
+
+  describe("toDatePath", () => {
     it("TODO: base cases");
     it("TODO: general cases");
   });
